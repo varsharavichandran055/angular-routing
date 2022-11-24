e11cc18de95018f6e21ab5f09dc480a173fdbee7
varsharavichandran055 <varsharavichandran055>
2022-11-24 22:23:00 +0530

    Wildcard routes for page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { CoursesComponent } from './courses/courses.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path:'courses',component:CoursesComponent},
-  {path:'contact',component:ContactComponent}
+  {path:'contact',component:ContactComponent},
+  {path:'**',component:PageNotFoundComponent}
 ];
 
 @NgModule({
