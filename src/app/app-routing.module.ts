14e4c98e4a70cc3b1a66272a07d473049565b2e8
varsharavichandran055 <varsharavichandran055>
2022-11-24 20:43:51 +0530

    angular component create and routing done in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { CoursesComponent } from './courses/courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'courses',component:CoursesComponent},
+  {path:'contact',component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { ContactComponent } from './contact/contact.component';
+import { CoursesDetailComponent } from './courses-detail/courses-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    ContactComponent,
+    CoursesDetailComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
